08a5c269efb26a9cf0e0e95b7407eeb66ceadc34
ProteusSeed <stanlin.caceres@gmail.com>
2018-06-26 19:58:28 -0400

    Cleanned up controller

--- WorkoutApp/Controllers/WorkoutController.cs
+++ WorkoutApp/Controllers/WorkoutController.cs
@@ -17,28 +17,14 @@ namespace WorkoutApp.Web.Controllers
             this.WorkoutExcerciseRepo = WorkoutExcerciseRepo;
         }
 
-        [HttpPost("CreateWorkoutExcercise")]///{Set_Number},{Rep_Number},{Workout_Excercise_Note}")]
-        public Workout_Excercise CreateWorkoutExcercise([FromBody]Workout_Excercise Data) //(int Program_Version_Id, int Excercise_Id, int Weight, int Set_Number, int Rep_Number, string Workout_Excercise_Note)
+        [HttpPost("CreateWorkoutExcercise")]
+        public Workout_Excercise CreateWorkoutExcercise([FromBody]Workout_Excercise Data) 
         {
-            //Workout_Excercise WorkoutExcercise = new Workout_Excercise(){
-            //    Workout_DateTime = DateTime.Now,
-            //    Program_Version_Id = Program_Version_Id,
-            //    Excercise_Id = Excercise_Id,
-            //    Weight = Weight,
-            //    Set_Number = Set_Number,
-            //    Rep_Number = Rep_Number,
-            //    Workout_Excercise_Note = "testing"
-            //};
-         
+
             WorkoutExcerciseRepo.CreateWorkout(Data);
 
             return Data;
         }
 
-        [HttpGet("TestGet")]
-        public IActionResult TestGet()
-        {
-            return Ok();
-        }
     }
 }
