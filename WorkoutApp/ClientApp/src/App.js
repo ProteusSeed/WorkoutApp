2b5b05c03a6c4ba24beb387151d3ef0f10f221cf
ProteusSeed <stanlin.caceres@gmail.com>
2018-06-29 19:16:03 -0400

    Form before utilizing axios api call

--- WorkoutApp/ClientApp/src/App.js
+++ WorkoutApp/ClientApp/src/App.js
@@ -6,36 +6,43 @@ import './components/WorkoutAppStyles.css';
 //import { FetchData } from './components/FetchData';
 //import { Counter } from './components/Counter';
 
+class WorkoutExcerciseForm extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+                return (
+                    <div >
+                        WORKOUT
+                            <form id="frmWorkoutExcercises" className="formInput">
+
+                            <select id="Excercises" form="frmWorkoutExcercises">
+                                <option value="1">Squat</option>
+                                <option value="2">Bench Press</option>
+                                <option value="3">Dead Lift</option>
+                                <option value="4">Military Press</option >
+                            </select>
+
+                            <label>Set Number</label>
+                            <input type="number" name="txtSetNumber" />
+
+                            <label>Weight</label>
+                            <input type="number" name="txtWeight" />
+
+                            <label>Reps</label>
+                            <input type="number" name="txtNumberOfReps" />
+
+                            <button>Send data!</button>
+                        </form>
+                    </div>
+                );
+    }
+}
+
 export default class App extends Component {
   displayName = App.name
 
   render() {
-              return (
-                  <div >  
-                      WORKOUT
-                          <form className="formInput"> 
-                              <label>Excercise</label>
-                              <input list="Excercises" name="ExcerciseId" id="txtExcerciseId"/>
-                          <datalist id="Excercises">
-                                      <option value="Internet Explorer"/>
-                                          <option value="Firefox"/>
-                                              <option value="Chrome"/>
-                                                  <option value="Opera"/>
-                                                      <option value="Safari"/>
-                                    </datalist> 
-
-                              <label>Set Number</label>
-                              <input type="number" name="txtSetNumber" />
-                         
-                              <label>Weight</label>
-                              <input type="number" name="txtWeight" />
-                    
-                              <label>Reps</label>
-                              <input type="number" name="txtNumberOfReps" />
-                        
-                              <button>Send data!</button>
-                          </form>
-                        </div>
-            );
+            return <WorkoutExcerciseForm/>
           }
 }
